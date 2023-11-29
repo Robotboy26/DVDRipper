b072409ea6899a99dc3866af5be8144c5b0593d9
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-11-29 10:15:55 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,23 @@
-import dvdread
 import sys
+import subprocess
 
 # Specify the path to the input DVD
 if len(sys.argv) > 1:
     dvd_path = sys.argv[1]
 else:
-    quit("you need to provide the path")
+    quit("You need to provide the path")
 
-# Open the DVD using pydvdread
-dvd = pydvdread.open(dvd_path)
-
-# Access the DVD contents
-for title in dvd:
-    print(f"Title: {title.title_number}, Duration: {title.duration}s")
-
-# Extract specific title to a file
+# Extract specific title using HandBrake CLI
 desired_title = 1
-output_file = '/home/output.mkv'
+output_file = 'output.mkv'
 
-with open(output_file, 'wb') as f:
-    dvd.extract_title(desired_title, f)
-    print("Successfully extracted title to file.")
+handbrake_cmd = [
+    'HandBrakeCLI',
+    '-i', dvd_path,
+    '-Z', 'H.265 MKV 2160p60',
+    '-o', output_file,
+]
 
-# Close the DVD
-dvd.close()
+subprocess.run(handbrake_cmd)
+
+print("Successfully extracted title to file.")
