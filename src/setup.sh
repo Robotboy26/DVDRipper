e8639443f090dadda4a874726b5cc8b539ba31e3
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-11-29 10:35:56 -0800

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -7,7 +7,7 @@ gunzip makemkv-oss-1.17.5.tar.gz
 tar -xf makemkv-bin-1.17.5.tar -C .
 tar -xf makemkv-oss-1.17.5.tar -C .
 rm makemkv-bin-1.17.5.tar makemkv-oss-1.17.5.tar 
-sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev
+sudo apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev -y
 cd makemkv-oss-1.17.5
 ./configure
 make
