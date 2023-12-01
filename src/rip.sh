b507c41f4d678f55100da781de19d07046653639
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-11-30 17:17:30 -0800

    Update rip.sh

--- src/rip.sh
+++ src/rip.sh
@@ -26,7 +26,7 @@ for index in "${!list[@]}"; do
 	rm read${list[index]}.txt
 	makemkvcon mkv ./rom${list[index]} 1 "./$content" > "$content/$content.rip.log"
  	blocks=$(isosize -d 2048 /dev/sr0)
-  	touch "/$content/iso/$content.iso"
+  	touch "$content/iso/$content.iso"
  	sudo dd if=/dev/sr0 "of=/$content/iso/$content.iso" bs=2048 count=$blocks status=progress
 done
 
