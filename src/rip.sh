53421ee8ff9d372b3fc13260760ddae297ef080c
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-12-01 07:33:55 -0800

    Update rip.sh

--- src/rip.sh
+++ src/rip.sh
@@ -5,7 +5,7 @@
 list=(1)
 
 for index in "${!list[@]}"; do
-    makemkvcon info ./rom${list[index]} -r > read${list[index]}.txt
+    makemkvcon info /dev/sr${list[index]} -r > read${list[index]}.txt
 
 	while IFS= read -r line; do
 	    if [[ $line == *"CINFO:2,0,"* ]]; then
@@ -20,10 +20,10 @@ for index in "${!list[@]}"; do
  	mkdir "$content/iso"
 	cp read${list[index]}.txt "$content/$content.info.log"
 	rm read${list[index]}.txt
-	makemkvcon mkv ./rom${list[index]} 0 "./$content" > "$content/$content.rip.log"
+	makemkvcon mkv /dev/sr${list[index]} 0 "./$content" > "$content/$content.rip.log"
  	blocks=$(isosize -d 2048 /dev/sr0)
   	touch "$content/iso/$content.iso"
- 	sudo dd if=/dev/sr0 "of=$content/iso/$content.iso" bs=2048 count=$blocks status=progress
+ 	sudo dd if=/dev/sr${list[index]} "of=$content/iso/$content.iso" bs=2048 count=$blocks status=progress
 done
 
 #makemkvcon mkv disc:0 all ./test
