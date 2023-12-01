34e1cbc16129bc5b4212123acdcf11ddedaf7b66
Robotboy26 <113060096+Robotboy26@users.noreply.github.com>
2023-11-30 16:45:55 -0800

    Update rip.sh

--- src/rip.sh
+++ src/rip.sh
@@ -25,7 +25,7 @@ for index in "${!list[@]}"; do
 	cp read${list[index]}.txt "$content/$content.info.log"
 	rm read${list[index]}.txt
 	makemkvcon mkv ./rom${list[index]} 1 "./$content" > "$content/$content.rip.log"
- 	sudo dd if=/dev/sr0 of=/$content/$content\ backup/$content.iso bs=2048 count=$blocks status=progress
+ 	sudo dd if=/dev/sr0 of="/$content/$content backup/$content.iso" bs=2048 count=$blocks status=progress
 done
 
 #makemkvcon mkv disc:0 all ./test
