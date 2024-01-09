ea55f7ef3b85f22cf2673364e759dbc364928566
robotboy26 <robotboygamer26@gmail.com>
2024-01-09 03:53:50 +0000

    Changes to be committed:

--- src/rip.sh
+++ src/rip.sh
@@ -1,20 +1,31 @@
-list=(0)
+dest=$"/home/robotserver/external/jellyfin/movies/"
 
-for index in "${!list[@]}"; do
-    dvdbackup -i /dev/sr${list[index]} -I -v > read${list[index]}.txt
+sudo apt install eject gddrescue -y
 
-  content=$(grep 'DVD-Video information of the DVD with title' "read${list[index]}.txt" | sed 's/.*"\(.*\)"/\1/')
-  echo "$content is now being ripped"
+process_dvd() {
+	local number=$1
+	dvdbackup -i /dev/sr$number -I -v > read$number.txt
 
-	mkdir "$content"
- 	mkdir "$content/iso"
-	cp read${list[index]}.txt "$content/$content.info.log"
-	rm read${list[index]}.txt
-  dvdbackup -i /dev/sr${list[index]} -o "$content" -F -p
-  # Convert the ripped track to MKV using ffmpeg
-  output_file="$content/$content.mkv"
-  # now get the main title in mkv format
- 	blocks=$(isosize -d 2048 /dev/sr0)
-  touch "$content/iso/$content.iso"
- 	sudo dd if=/dev/sr${list[index]} "of=$content/iso/$content.iso" bs=2048 count=$blocks status=progress
-done
+	content=$(grep 'DVD-Video information of the DVD with title' "read$number.txt" | sed 's/.*"\(.*\)"/\1/')
+	echo "$content is now being ripped"
+	if [ -z "$content" ]; then
+		echo "There is no dvd"
+		exit
+	fi
+
+	sudo mkdir "$dest$content"
+ 	sudo mkdir "$dest$content/iso"
+	sudo cp read$number.txt "$dest$content/$content.info.log"
+	sudo rm read$number.txt
+	# dvdbackup -i /dev/sr${list[index]} -o "$content" -F -p
+ 	# blocks=$(isosize -d 2048 /dev/sr0)
+	sudo touch "$dest$content/iso/$content.iso"
+	isoLoc=$"$dest$content/iso/$content.iso"
+ 	sudo ddrescue /dev/sr$number "$isoLoc"
+	echo "downloaded the iso file for $content into $isoLoc in location $dest"
+	eject /dev/sr$number -F
+	sudo makemkvcon mkv "$isoLoc" 0 "$dest$content"
+}
+
+process_dvd 0
+process_dvd 1
